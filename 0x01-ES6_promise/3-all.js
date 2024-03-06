2d2c46d621a4812aa2495dad946d77f9fb3f1597
Mohamed AJJAJI <majjaji98@gmail.com>
2024-03-06 15:03:30 +0000

    Error fixed

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -6,6 +6,6 @@ export default function handleProfileSignup() {
       console.log(`${photo.body} ${user.firstName} ${user.lastName}`);
     })
     .catch(() => {
-      console.error('Signup system offline');
+      console.log('Signup system offline');
     });
 }
